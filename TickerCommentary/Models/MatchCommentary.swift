76fb942cadeeb751420808ba8cc7bb863a0ef274
Jacob <jacob_fafiolu@hotmail.co.uk>
2020-07-29 13:42:15 +0200

    [Commentary] - Added match commentary & match photos models

---
+++ TickerCommentary/Models/MatchCommentary.swift
@@ -0,0 +1,17 @@
+//
+//  Commentary.swift
+//  TickerCommentary
+//
+//  Created by user on 29/07/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
+
+struct MatchCommentary: Codable {
+    
+    var id: String
+    var heading: String
+    var subheading: String
+    
+}
---
+++ TickerCommentary/Models/MatchPhotos.swift
@@ -0,0 +1,15 @@
+//
+//  MatchPhotos.swift
+//  TickerCommentary
+//
+//  Created by user on 29/07/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
+
+struct MatchPhotos: Codable {
+    
+    var id: String
+    var photoUrl: String
+}
