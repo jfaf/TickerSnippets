2e65077d932aafb41a465699964dfe0ecba4471a
Jacob <jacob_fafiolu@hotmail.co.uk>
2020-07-29 11:57:44 +0200

    Initial Commit

---
+++ TickerCommentary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TickerCommentary
+//
+//  Created by user on 29/07/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
