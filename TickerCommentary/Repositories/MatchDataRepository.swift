2823578d850a90e165cdaa4f4b8fe374c712f318
Jacob <jacob_fafiolu@hotmail.co.uk>
2020-07-29 17:53:15 +0200

    [Commentary] Added repo to get test data

---
+++ TickerCommentary/Repositories/MatchDataRepository.swift
@@ -0,0 +1,32 @@
+//
+//  MatchDataRepository.swift
+//  TickerCommentary
+//
+//  Created by user on 29/07/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import SwiftyJSON
+
+class MatchDataRepository {
+    
+    func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name,
+                                                 ofType: "json"),
+                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+    
+}
+
+
